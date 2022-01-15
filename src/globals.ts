15eff1c7aab8efd1e5adb5d070cb31826b58951d
Macsik121 <hiomimipad@gmail.com>
2022-01-15 23:08:06 +0300

    api endpoints are changed

--- src/globals.ts
+++ src/globals.ts
@@ -4,7 +4,7 @@ export default {
         'http://localhost:3000/graphql'
     ),
     __UI_SERVER_ENDPOINT__: (
-        'https://macsik121s-first-chat.herokuapp.com' ||
+        'https://macsik121s-first-chat.herokuapp.com'
         // 'http://localhost:8000'
     )
 }
